e72364127348cc861d8a95dc884e40437fef2f5d
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-06-25 18:44:23 +0200

    solved 5 of 6 tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,13 @@
 const leapYears = function(leapYear) {
 
-    if (leapYear % 4 === 0 || leapYear % 400 === 0) {
-        return true;
-    } else if (leapYear === 1900) {
+    if (leapYear % 100 === 0 || leapYear % 400 === 0) {
         return false;
     }
-    
-    else
-        return false;
+    if (leapYear % 4 === 0 || leapYear % 400 === 0) {
+        return true;
+    } else 
+    return false;
+
 
     // pseudocode
     // if (leapYear / 4) (!&& leapYer / 100 !|| leapYear / 400)
