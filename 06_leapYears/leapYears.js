707283013303409c190ba5fd33555b05a8c0d4eb
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-06-25 12:59:36 +0200

    Start on leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
 const leapYears = function() {
+    num = 1984
+    test = num / 4;
+    console.log(test);
 
+    // pseudocode
+    // if (leapYear / 4) (!&& leapYer / 100 !|| leapYear / 400)
+    // return true
 };
 
 // Do not edit below this line
