fa608a6254dac5346fab417e4d2f0ee5b3d395e9
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-06-25 12:07:31 +0200

    copied Jaybees techtalk walkthrough on youtube to see the solution in practice

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,21 @@
-const sumAll = function(...args) {
-    const sum = [1, 2, 3, 4].reduce((partialSum, a) => partialSum + a, 0);
-    return sum;
-    }
+const sumAll = function(num1, num2) {
+    let error = 'ERROR';
+    let finalSum = 0;
+
+    let biggerValue = Math.max(num1, num2);
+    let smallerValue = Math.min(num1, num2);
+
+    if (((typeof num1 === 'string')) || (typeof num2 === 'string')) {
+        return error;
+    } else if (((typeof num1 === 'object')) || (typeof num2 === 'object')) {
+        return error;
+    } else if ((biggerValue >= 0) && (smallerValue >= 0)) {
+        for (let i = smallerValue; i <= biggerValue; i++) {
+            finalSum = finalSum + i;
+        }
+    } else return error;
+    return finalSum;
+}
 
 
 // Do not edit below this line
