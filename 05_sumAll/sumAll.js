832466a8c7c39889ea093935780997ee673c00b6
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-06-25 08:39:26 +0200

    first commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num) {
+    sum = num + num;
+    return sum;
+    console.log(1+4 + " : Sum of, 1 + 4.");
+    console.log(1+2+3+4 + " : Sum of, 1 + 2 + 3 + 4.");
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
