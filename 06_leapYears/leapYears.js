fe58f9bb8d171b876c8d7cd1325c6a8bf1f48dca
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-06-26 11:24:09 +0200

    Continued work on leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,14 @@
 const leapYears = function(leapYear) {
 
-    if (leapYear === 1600) {
-        return true;
-    }
+    return (year % 4 === 0 &&  year % 100 !== 0 || year % 400 === 0);
 
-    if (leapYear % 100 === 0 || leapYear % 400 === 0) {
-        return false;
-    }
-    if (leapYear % 4 === 0 || leapYear % 400 === 0) {
-        return true;
-    } else 
-    return false;
+
+    // if (leapYear % 4 === 0 && leapYear % 100 !== 0) {
+    //     return true;
+    // }
+    // else if (leapYear % 400 === 0) {
+    //     return false;
+    // }
 
 
     // pseudocode
