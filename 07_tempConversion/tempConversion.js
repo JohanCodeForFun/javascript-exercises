7d25d71ecaa91bdcdfab58d82debaf08cd24b039
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-06-26 11:24:50 +0200

    Wrote the conversion math, rounding and successfully past the first tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let fahrenheitConversion = (((fahrenheit - 32) * 5) / 9);
+  let fahrenheitConversionRounded = Math.round(fahrenheitConversion * 10) / 10;
+  console.log(fahrenheitConversionRounded);
+  return fahrenheitConversionRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let celciusConversion = (((celcius * 9) / 5) + 32);
+  let celciusConversionRounded = Math.round(celciusConversion * 10) / 10;
+  console.log(celciusConversionRounded);
+  return celciusConversionRounded;
 };
 
 // Do not edit below this line
