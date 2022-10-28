a9b8e6e01e86f2703b15b226fd603c8f813a80d3
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-28 21:21:58 +0200

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = '';
+    str.toLowerCase();
 
+    for (let i = str.length -1; i >= 0; i--) {
+       newStr += str[i];
+    }
+    console.log(str);
+    console.log(newStr);
+    if (str === newStr) {
+        return true;
+    } else {
+        return false
+    }
 };
+
+console.log(palindromes("a race car"))
+console.log(palindromes("Racecar!"))
+console.log(palindromes("A car, a man, a maraca."))
+console.log(palindromes("Animal loots foliated detail of stool lamina."))
+console.log(palindromes("A car, a man, a maraca."))
 
 // Do not edit below this line
 module.exports = palindromes;
