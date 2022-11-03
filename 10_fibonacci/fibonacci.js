5dd4118b6e824619e6507ba73b0400263822854f
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-11-03 21:51:21 +0100

    write function to get the n member of fibonacci family

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = num => {
+  let fiboArr = [];
+  let sum = 0;
+  fiboArr.push(1, 1);
 
+  if (Math.sign(num) === -1) {
+    return "OOPS";
+  }
+  for (let i = 1; i < num; i++) {
+    sum = fiboArr[i - 1] + fiboArr[i];
+    fiboArr.push(sum);
+  }
+  
+  return fiboArr[fiboArr.length - 2];
 };
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
