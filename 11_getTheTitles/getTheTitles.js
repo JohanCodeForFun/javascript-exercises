e6b22935467504b6fa19eb7e400d921885d67869
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-11-03 22:16:29 +0100

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = books => {
+    let bookTitles = [];
+    books.forEach(book => bookTitles.push(book.title))
+    return bookTitles;
 };
+
+// for testing purpose
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
